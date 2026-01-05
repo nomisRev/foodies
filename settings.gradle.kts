80ad3e88c786c99878bfccc9d220326f8332cd2a
Simon Vergauwen <nomisRev@users.noreply.github.com>
2026-01-05 19:19:32 +0100

    Add Spotless plugin for code formatting, inline Gradle configurations, and format code.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,10 @@
 dependencyResolutionManagement {
-    @Suppress("UnstableApiUsage")
-    repositories {
-        mavenCentral()
-    }
+    @Suppress("UnstableApiUsage") repositories { mavenCentral() }
 
-    versionCatalogs {
-        create("ktorLibs") {
-            from("io.ktor:ktor-version-catalog:3.3.3")
-        }
-    }
+    versionCatalogs { create("ktorLibs") { from("io.ktor:ktor-version-catalog:3.3.3") } }
 }
 
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
+plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0" }
 
 include(":server")
 
