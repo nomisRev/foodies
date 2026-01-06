27f25eed98651353081b9a1159833e669236889d
Simon Vergauwen <nomisRev@users.noreply.github.com>
2026-01-06 14:17:31 +0100

    Remove KtFmt (Spotless) plugin and update formatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,3 @@
-import com.diffplug.spotless.kotlin.KtfmtStep
-
 plugins {
-    alias(libs.plugins.spotless)
     alias(libs.plugins.kotlin.assert)
-}
-
-spotless {
-    kotlin {
-        target("**/*.kt", "**/*.kts")
-        ktfmt(libs.versions.ktfmt.get()).kotlinlangStyle().configure {
-            it.setTrailingCommaManagementStrategy(
-                KtfmtStep.TrailingCommaManagementStrategy.ONLY_ADD
-            )
-            it.setRemoveUnusedImports(true)
-        }
-    }
 }
