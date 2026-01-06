541844af4abaec99643332e06af28652c72b85df
Simon Vergauwen <nomisRev@users.noreply.github.com>
2026-01-06 13:14:55 +0100

    Rename `Env` to `Config`, and cleanup before starting package-by-feature organisation.

--- server/src/main/kotlin/io/ktor/foodies/server/Module.kt
+++ server/src/main/kotlin/io/ktor/foodies/server/Module.kt
@@ -1,5 +1,27 @@
 package io.ktor.app.io.ktor.foodies.server
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import io.ktor.server.application.Application
+import io.ktor.server.application.ApplicationStopped
 import org.jetbrains.exposed.v1.jdbc.Database
+import org.jetbrains.exposed.v1.jdbc.transactions.TransactionManager
 
 class Module(val database: Database)
+
+fun Application.database(database: Config.DataSource): Database {
+    val hikari =
+        HikariDataSource(
+            HikariConfig().apply {
+                jdbcUrl = database.url
+                username = database.username
+                password = database.password
+            }
+        )
+    val db = Database.connect(hikari)
+    monitor.subscribe(ApplicationStopped) {
+        TransactionManager.closeAndUnregister(db)
+        hikari.close()
+    }
+    return db
+}
