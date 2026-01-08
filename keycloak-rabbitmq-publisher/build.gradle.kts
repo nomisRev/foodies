ebc6224439a971cca93859f0a310676ffc9d720e
Simon Vergauwen <nomisRev@users.noreply.github.com>
2026-01-08 13:04:05 +0100

    Add Keycloak test for publishing User Registration Events to RabbitMQ, upgrade to JDK 21 and Keycloak 26, and improve shared test utilities.

--- keycloak-rabbitmq-publisher/build.gradle.kts
+++ keycloak-rabbitmq-publisher/build.gradle.kts
@@ -1,11 +1,15 @@
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.serialization)
+    alias(libs.plugins.testballoon)
     id("com.gradleup.shadow") version "8.3.6"
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
+    compilerOptions { freeCompilerArgs.add("-Xcontext-parameters") }
 }
 
 dependencies {
@@ -16,9 +20,21 @@ dependencies {
 
     implementation(ktorLibs.serialization.kotlinx.json)
     implementation(libs.rabbitmq)
+
+    testImplementation(libs.testballoon)
+    testImplementation(libs.keycloak.core)
+    testImplementation(libs.keycloak.services)
+    testImplementation(libs.keycloak.server.spi)
+    testImplementation(libs.keycloak.server.spi.private)
+    testImplementation(project(":server-shared-test"))
 }
 
 tasks.shadowJar {
-    archiveClassifier.set("") // Replaces the default JAR
-    mergeServiceFiles() // Important: merges META-INF/services files
+    archiveClassifier.set("")
+    mergeServiceFiles()
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+    testLogging { events(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED) }
 }
---
+++ keycloak-rabbitmq-publisher/src/test/kotlin/io/ktor/foodies/keycloak/Utils.kt
@@ -0,0 +1,60 @@
+package io.ktor.foodies.keycloak
+
+import com.rabbitmq.client.ConnectionFactory
+import de.infix.testBalloon.framework.core.TestConfig
+import de.infix.testBalloon.framework.core.TestSuite
+import de.infix.testBalloon.framework.core.testSuite
+import de.infix.testBalloon.framework.shared.TestDisplayName
+import de.infix.testBalloon.framework.shared.TestElementName
+import de.infix.testBalloon.framework.shared.TestRegistering
+import io.ktor.foodies.server.test.RabbitContainer
+import io.ktor.foodies.server.test.rabbitContainer
+import kotlinx.serialization.json.Json
+
+val json = Json { encodeDefaults = true; ignoreUnknownKeys = true }
+
+internal fun RabbitContainer.config(queueName: String) = RabbitConfig(
+    host,
+    amqpPort,
+    adminUsername,
+    adminPassword,
+    queueName
+)
+
+class RabbitContext(
+    val container: TestSuite.Fixture<RabbitContainer>,
+    val factory: TestSuite.Fixture<ConnectionFactory>
+)
+
+context(ctx: RabbitContext)
+val container: TestSuite.Fixture<RabbitContainer>
+    get() = ctx.container
+
+context(ctx: RabbitContext)
+val factory: TestSuite.Fixture<ConnectionFactory>
+    get() = ctx.factory
+
+@TestRegistering
+context(ctx: RabbitContext)
+internal fun TestSuite.testListener(name: String, block: suspend (String, ProfileWebhookEventListener) -> Unit) =
+    test(name) {
+        val queueName = name.split(" ").joinToString(separator = ".").lowercase()
+        val listener = ProfileWebhookEventListener(ctx.container().config(queueName))
+        try {
+            block(queueName, listener)
+        } finally {
+            listener.close()
+        }
+    }
+
+@TestRegistering
+fun rabbitSuite(
+    @TestElementName name: String = "",
+    @TestDisplayName displayName: String = name,
+    testConfig: TestConfig = TestConfig,
+    content: context(RabbitContext) TestSuite.() -> Unit
+): Lazy<TestSuite> = testSuite(name, displayName, testConfig) {
+    val container: TestSuite.Fixture<RabbitContainer> = rabbitContainer()
+    val factory: TestSuite.Fixture<ConnectionFactory> = testFixture { container().connectionFactory() }
+    content(RabbitContext(container, factory), this)
+}
