f43346019577cac06611739ebb1c29ade8697fc7
Simon Vergauwen <nomisRev@users.noreply.github.com>
2026-01-12 15:39:16 +0100

    Add ProfileApp with Cohort-based health checks and setup readiness checks for Hikari and RabbitMQ connections

--- profile/src/main/kotlin/io/ktor/foodies/server/ProfileModule.kt
+++ profile/src/main/kotlin/io/ktor/foodies/server/ProfileModule.kt
@@ -1,5 +1,7 @@
 package io.ktor.foodies.server
 
+import com.sksamuel.cohort.HealthCheckRegistry
+import com.sksamuel.cohort.hikari.HikariConnectionsHealthCheck
 import io.ktor.foodies.rabbitmq.Consumer
 import io.ktor.foodies.rabbitmq.RabbitConfig
 import io.ktor.foodies.rabbitmq.channel
@@ -9,9 +11,15 @@ import io.ktor.foodies.server.consumers.userEventConsumer
 import io.ktor.foodies.server.profile.ExposedProfileRepository
 import io.ktor.foodies.user.event.UserEvent
 import io.ktor.server.application.Application
+import kotlinx.coroutines.Dispatchers
 import org.flywaydb.core.Flyway
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
 
-class ProfileModule(val consumers: List<Consumer>)
+class ProfileModule(
+    val consumers: List<Consumer>,
+    val readinessCheck: HealthCheckRegistry
+)
 
 fun Application.module(config: Config): ProfileModule {
     val dataSource = dataSource(config.dataSource)
@@ -28,6 +36,7 @@ fun Application.module(config: Config): ProfileModule {
         password = config.rabbit.password
     )
     val connectionFactory = rabbitConnectionFactory(rabbitConfig)
+    val connection = connectionFactory.newConnection()
     val newUserConsumer =
         userEventConsumer(
             connectionFactory.channel(config.rabbit.queue, "profile-service")
@@ -35,5 +44,13 @@ fun Application.module(config: Config): ProfileModule {
             profileRepository
         )
 
-    return ProfileModule(listOf(newUserConsumer))
+    val readinessCheck = HealthCheckRegistry(Dispatchers.IO) {
+        register(HikariConnectionsHealthCheck(dataSource.hikari, 1), Duration.ZERO, 5.seconds)
+        register(RabbitConnectionHealthCheck(connection), Duration.ZERO, 5.seconds)
+    }
+
+    return ProfileModule(
+        consumers = listOf(newUserConsumer),
+        readinessCheck = readinessCheck
+    )
 }
