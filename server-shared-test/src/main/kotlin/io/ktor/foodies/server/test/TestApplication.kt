60e2ae4c12c378660feda23b2d58e5f4cdff2839
Simon Vergauwen <nomisRev@users.noreply.github.com>
2026-01-12 10:47:28 +0100

    Add `jsonClient` helper to simplify client creation with ContentNegotiation plugin

--- server-shared-test/src/main/kotlin/io/ktor/foodies/server/test/TestApplication.kt
+++ server-shared-test/src/main/kotlin/io/ktor/foodies/server/test/TestApplication.kt
@@ -3,6 +3,8 @@ package io.ktor.foodies.server.test
 import de.infix.testBalloon.framework.core.TestExecutionScope
 import de.infix.testBalloon.framework.core.TestSuite
 import de.infix.testBalloon.framework.shared.TestRegistering
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.testing.ApplicationTestBuilder
 import io.ktor.server.testing.testApplication
 
@@ -11,3 +13,7 @@ fun TestSuite.testApplication(
     name: String,
     block: suspend context(TestExecutionScope) ApplicationTestBuilder.() -> Unit,
 ) = test(name) { testApplication { block() } }
+
+fun ApplicationTestBuilder.jsonClient() = createClient {
+    install(ContentNegotiation) { json() }
+}
