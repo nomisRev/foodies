cda3da535bc9f5f3c195820c9035a1ea551f05c7
Simon Vergauwen <nomisRev@users.noreply.github.com>
2026-01-12 15:39:26 +0100

    Add health check specs for ProfileApp using `ctxSuite` to test liveness and readiness probes with database and RabbitMQ connectivity

---
+++ profile/src/test/kotlin/io/ktor/foodies/server/HealthCheckSpec.kt
@@ -0,0 +1,34 @@
+package io.ktor.foodies.server
+
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import io.ktor.foodies.server.test.ctxSuite
+import io.ktor.http.HttpStatusCode
+import kotlin.test.assertEquals
+import kotlin.test.assertContains
+
+val healthCheckSpec by ctxSuite(context = { serviceContext() }) {
+
+    testProfileService("liveness probe returns 200 OK with ThreadDeadlockHealthCheck status") {
+        val response = client.get("/healthz/liveness")
+        assertEquals(HttpStatusCode.OK, response.status)
+        val body = response.bodyAsText()
+        assertContains(body, "thread", ignoreCase = true)
+    }
+
+    testProfileService("readiness probe checks database connectivity") {
+        val response = client.get("/healthz/readiness")
+        assertEquals(HttpStatusCode.OK, response.status)
+
+        val body = response.bodyAsText()
+        assertContains(body, "hikari_open_connections", ignoreCase = true)
+    }
+
+    testProfileService("readiness probe checks database connectivity") {
+        val response = client.get("/healthz/readiness")
+        assertEquals(HttpStatusCode.OK, response.status)
+
+        val body = response.bodyAsText()
+        assertContains(body, "rabbitmq", ignoreCase = true)
+    }
+}
---
+++ profile/src/test/kotlin/io/ktor/foodies/server/TestSyntax.kt
@@ -0,0 +1,64 @@
+package io.ktor.foodies.server
+
+import com.rabbitmq.client.ConnectionFactory
+import de.infix.testBalloon.framework.core.TestExecutionScope
+import de.infix.testBalloon.framework.core.TestSuite
+import de.infix.testBalloon.framework.shared.TestRegistering
+import io.ktor.foodies.server.test.PostgreSQLContainer
+import io.ktor.foodies.server.test.RabbitContainer
+import io.ktor.foodies.server.test.dataSource
+import io.ktor.foodies.server.test.postgresContainer
+import io.ktor.foodies.server.test.rabbitContainer
+import io.ktor.foodies.server.test.testApplication
+import io.ktor.server.testing.ApplicationTestBuilder
+
+data class ServiceContext(
+    val postgreSQLContainer: TestSuite.Fixture<PostgreSQLContainer>,
+    val dataSource: TestSuite.Fixture<DataSource>,
+    val rabbitContainer: TestSuite.Fixture<RabbitContainer>,
+    val connectionFactory: TestSuite.Fixture<ConnectionFactory>,
+)
+
+fun TestSuite.serviceContext(): ServiceContext {
+    val container = postgresContainer()
+    val dataSource = testFixture {
+        val dataSource = container().dataSource()()
+        dataSource
+    }
+    val rabbitContainer = rabbitContainer()
+    val connectionFactory = testFixture { rabbitContainer().connectionFactory() }
+    return ServiceContext(container, dataSource, rabbitContainer, connectionFactory)
+}
+
+@TestRegistering
+context(ctx: ServiceContext)
+fun TestSuite.testProfileService(
+    name: String,
+    block: suspend context(TestExecutionScope) ApplicationTestBuilder.() -> Unit
+) {
+    testApplication(name) {
+        application {
+            app(
+                module(
+                    Config(
+                        host = "0.0.0.0",
+                        port = 8080,
+                        dataSource = DataSource.Config(
+                            ctx.postgreSQLContainer().jdbcUrl,
+                            ctx.postgreSQLContainer().username,
+                            ctx.postgreSQLContainer().password
+                        ),
+                        rabbit = Config.Rabbit(
+                            ctx.rabbitContainer().host,
+                            ctx.rabbitContainer().amqpPort,
+                            ctx.rabbitContainer().adminUsername,
+                            ctx.rabbitContainer().adminPassword,
+                            "profile-queue"
+                        )
+                    )
+                )
+            )
+        }
+        block()
+    }
+}
