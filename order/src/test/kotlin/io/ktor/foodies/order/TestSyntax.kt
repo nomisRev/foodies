7c447d7d59eef81c906d0058671204e21197b0cd
Simon Vergauwen <nomisRev@users.noreply.github.com>
2026-01-13 13:36:45 +0100

    Remove `IdempotencyService` and related dependencies as they are no longer used across the Order module.

--- order/src/test/kotlin/io/ktor/foodies/order/TestSyntax.kt
+++ order/src/test/kotlin/io/ktor/foodies/order/TestSyntax.kt
@@ -1,54 +1,34 @@
 package io.ktor.foodies.order
 
-import de.infix.testBalloon.framework.core.TestSuite
 import io.ktor.foodies.order.client.BasketClient
 import io.ktor.foodies.order.client.CustomerBasket
 import io.ktor.foodies.order.domain.*
-import io.ktor.foodies.order.repository.IdempotencyRepository
 import io.ktor.foodies.order.repository.OrderRepository
-import io.ktor.foodies.order.repository.ProcessedRequest
 import io.ktor.foodies.order.service.DefaultOrderService
-import io.ktor.foodies.order.service.IdempotencyService
 import io.ktor.foodies.order.service.OrderEventPublisher
-import io.ktor.foodies.server.test.ctxSuite
-import java.util.UUID
 import kotlin.time.Instant
 
 /**
  * Holds test dependencies for the OrderService.
  */
 data class TestContext(
-    val idempotencyRepository: InMemoryIdempotencyRepository,
     val orderRepository: InMemoryOrderRepository,
     val basketClient: InMemoryBasketClient,
     val eventPublisher: InMemoryOrderEventPublisher,
-    val idempotencyService: IdempotencyService,
     val service: DefaultOrderService
 )
 
 fun createTestContext(): TestContext {
-    val idempotencyRepository = InMemoryIdempotencyRepository()
-    val idempotencyService = IdempotencyService(idempotencyRepository)
     val orderRepository = InMemoryOrderRepository()
     val basketClient = InMemoryBasketClient()
     val eventPublisher = InMemoryOrderEventPublisher()
-    val service = DefaultOrderService(orderRepository, basketClient, eventPublisher, idempotencyService)
+    val service = DefaultOrderService(orderRepository, basketClient, eventPublisher)
     return TestContext(
-        idempotencyRepository = idempotencyRepository,
         orderRepository = orderRepository,
         basketClient = basketClient,
         eventPublisher = eventPublisher,
-        idempotencyService = idempotencyService,
         service = service
     )
-}
-
-class InMemoryIdempotencyRepository : IdempotencyRepository {
-    val requests = mutableMapOf<UUID, ProcessedRequest>()
-    override fun findByRequestId(requestId: UUID): ProcessedRequest? = requests[requestId]
-    override fun save(request: ProcessedRequest) {
-        requests[request.requestId] = request
-    }
 }
 
 class InMemoryOrderRepository : OrderRepository {
