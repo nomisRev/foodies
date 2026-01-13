017af152b0b76e7af1031b1d630e36f1c4c390a2
Simon Vergauwen <nomisRev@users.noreply.github.com>
2026-01-13 11:55:23 +0100

    Initialize E2E testing infrastructure and configuration

--- e2e/src/main/kotlin/com/foodies/e2e/utils/ApiHelpers.kt
+++ e2e/src/main/kotlin/com/foodies/e2e/utils/ApiHelpers.kt
@@ -1,17 +1,17 @@
 package com.foodies.e2e.utils
 
-import com.foodies.e2e.config.TestConfig
 import com.microsoft.playwright.APIRequest
 import com.microsoft.playwright.APIRequestContext
 import com.microsoft.playwright.Playwright
+import com.foodies.e2e.config.TestConfig
 
 object ApiHelpers {
     /**
      * Creates an APIRequestContext for making direct backend API calls.
      */
-    fun createApiContext(playwright: Playwright): APIRequestContext {
+    fun createApiContext(playwright: Playwright, config: TestConfig): APIRequestContext {
         return playwright.request().newContext(
-            APIRequest.NewContextOptions().setBaseURL(TestConfig.baseUrl)
+            APIRequest.NewContextOptions().setBaseURL(config.apiBaseUrl)
         )
     }
 }
