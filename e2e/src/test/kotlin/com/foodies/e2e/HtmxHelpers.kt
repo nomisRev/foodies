0a400de73f5726adac1acf4e0c47fab6f7f9f2c2
Simon Vergauwen <nomisRev@users.noreply.github.com>
2026-01-13 13:07:40 +0100

    Streamline E2E testing, and fix incorrect principal being requested in WebApp.

---
+++ e2e/src/test/kotlin/com/foodies/e2e/HtmxHelpers.kt
@@ -0,0 +1,8 @@
+package com.foodies.e2e
+
+import com.microsoft.playwright.ElementHandle
+import com.microsoft.playwright.Page
+import com.microsoft.playwright.options.WaitForSelectorState
+
+fun Page.waitForHtmxIdle(): ElementHandle? =
+    waitForSelector(".htmx-request", Page.WaitForSelectorOptions().setState(WaitForSelectorState.HIDDEN))
