f838568b5224b15d1997e4aa8f8e070c3f5be2e2
Simon Vergauwen <nomisRev@users.noreply.github.com>
2026-01-14 14:07:39 +0100

    Remove OpenIdConnectPrincipal implementation and refactor session handling to use userSession() in authenticated routes.

--- webapp/src/main/kotlin/io/ktor/foodies/server/security/UserSessionScope.kt
+++ webapp/src/main/kotlin/io/ktor/foodies/server/security/UserSessionScope.kt
@@ -1,28 +1,18 @@
 package io.ktor.foodies.server.security
 
-import io.ktor.http.HttpStatusCode
-import io.ktor.server.application.createRouteScopedPlugin
-import io.ktor.server.auth.AuthenticationChecked
-import io.ktor.server.response.respond
+import io.ktor.server.auth.authenticate
+import io.ktor.server.auth.principal
 import io.ktor.server.routing.Route
 import io.ktor.server.routing.RoutingContext
-import io.ktor.server.sessions.get
-import io.ktor.server.sessions.sessions
 
 fun interface UserSessionScope {
-    suspend fun RoutingContext.session(): UserSession
+    context(ctx: RoutingContext)
+    suspend fun userSession(): UserSession
 }
 
-inline fun Route.withUserSession(build: UserSessionScope.() -> Unit): Route = apply {
-    install(createRouteScopedPlugin("SessionAuth") {
-        on(AuthenticationChecked) { call ->
-            val session = call.sessions.get<UserSession>()
-            if (session == null) {
-                call.response.headers.append("HX-Redirect", "/login")
-                call.respond(HttpStatusCode.Unauthorized)
-            }
-        }
-    })
+context(scope: UserSessionScope)
+suspend fun RoutingContext.userSession(): UserSession = scope.userSession()
 
-    build(UserSessionScope { call.sessions.get<UserSession>()!! })
+fun Route.withUserSession(build: context(UserSessionScope) Route.() -> Unit): Route = authenticate {
+    build.invoke(UserSessionScope { contextOf<RoutingContext>().call.principal<UserSession>()!! }, this)
 }
