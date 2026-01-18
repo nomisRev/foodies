2578c7c69c4c6c0afea1859e6303d2a31e9db960
Simon Vergauwen <nomisRev@users.noreply.github.com>
2026-01-18 20:28:08 +0100

    feat: make OpenTelemetry OTLP endpoint configurable for Menu service

--- menu/src/main/kotlin/io/ktor/foodies/menu/Config.kt
+++ menu/src/main/kotlin/io/ktor/foodies/menu/Config.kt
@@ -10,7 +10,13 @@ data class Config(
     val port: Int,
     @SerialName("data_source") val dataSource: DataSource.Config,
     val rabbit: RabbitConfig,
-)
+    val telemetry: Telemetry,
+) {
+    @Serializable
+    data class Telemetry(
+        @SerialName("otlp_endpoint") val otlpEndpoint: String,
+    )
+}
 
 @Serializable
 data class RabbitConfig(
